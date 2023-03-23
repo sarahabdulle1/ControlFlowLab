fd1e80f1eb5833fd6ec2207c3a1b436d4f50998b
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-03-23 16:53:39 +0000

    Guessing game and the extension.

---
+++ java/org/example/Extension.java
@@ -0,0 +1,44 @@
+//Create a program that makes suggestions on what to wear based on information it gathers about the weather.
+package org.example;
+
+import java.util.Scanner;
+public class Extension {
+    public static void main(String[] args) {
+
+//  Prompt user to describe what the weather is like, giving parameters
+        System.out.println("Let's decide what to wear. Is the weather rainy, sunny, cloudy or snowy?");
+
+//  Collect the user input in a string format.
+        Scanner reader = new Scanner(System.in);
+        String describeWeather = reader.nextLine();
+        describeWeather.toLowerCase();
+
+//  Prompt the user to input the average temperature of the day.
+        System.out.println("Please enter the average temperature of the day in degrees celsius.");
+
+//  Collect the user input in integer format
+        Scanner secondreader = new Scanner(System.in);
+        String enterTemperatureString = secondreader.nextLine();
+        int enteredTemperature = Integer.valueOf(enterTemperatureString);
+
+//        If rainy AND above 15 degrees. Wear a light raincoat. Under 15 degrees. Wear a waterproof winter coat.
+//        If sunny AND above 18 degrees. Wear sunglasses. Under 18 degrees. Wear warm clothing. Do not be deceived by the sun!
+//        If cloudy AND above 22 degrees. Wear light clothing. If under. Dress warm.
+//        If snowy OR less than 2 degrees. Wear gloves and a winter coat.
+        if (describeWeather.equals("snowy") || enteredTemperature < 2) {
+            System.out.println("Wear gloves and a winter coat.");
+        } else if (enteredTemperature <= 15 && describeWeather.equals("rainy")) {
+            System.out.println("Wear a waterproof winter coat.");
+        } else if (enteredTemperature > 15 && describeWeather.equals("rainy")) {
+            System.out.println("Wear a light raincoat or bring an umbrella.");
+        } else if (enteredTemperature > 18 && describeWeather.equals("sunny")) {
+            System.out.println("Wear sunglasses");
+        } else if (enteredTemperature <= 18 && describeWeather.equals("sunny")) {
+            System.out.println("Wear warm clothing. Do not be deceived by the sun!");
+        } else if (enteredTemperature >= 22  && describeWeather.equals("cloudy")) {
+            System.out.println("Wear light clothing.");
+        } else if (enteredTemperature < 22 && describeWeather.equals("cloudy")) {
+            System.out.println("Dress warm.");
+        }
+    }
+}
---
+++ java/org/example/Main.java
@@ -0,0 +1,28 @@
+//Create a simple number guessing game, that takes in some user input matches it to a 'secret' number.
+
+package org.example;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//      Prompt user to 'What is the secret number? Take a guess...'
+        System.out.println("What is the secret number? Take your guess...");
+
+//      Collect user input. It must be a number.
+        Scanner reader = new Scanner(System.in);
+        String guessedNumberString = reader.nextLine();
+        int guessedNumber = Integer.valueOf(guessedNumberString);
+//       QUESTION: If the user inputs a decimal rather than a whole number, how would I convert it into a whole number?
+
+
+//      If the number is the correct number return 'Bingo!' If the number is incorrect return 'Not quite...'
+//      If the number is less than the secret number, return 'Higher'. If it is greater, return 'Lower'
+        if (guessedNumber == 85) {
+            System.out.println("Bingo!");
+        } else if (guessedNumber < 85) {
+            System.out.println("Not quite. Take a higher guess.");
+        } else if (guessedNumber > 85) {
+            System.out.println("Not quite. Take a lower guess.");
+        }
+    }
+}
